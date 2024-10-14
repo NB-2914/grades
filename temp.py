a8a616f483b50cca0a0b5ecd12f96a5c0f40f44e
NB-2914 <159939980+NB-2914@users.noreply.github.com>
2024-10-14 18:10:04 +0000

    delete

--- temp.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-text = "SELECT * FROM users WHERE username = 'Test'"
-conn = sqlite3.connect('grades.db')
-cursor = conn.cursor()
-cursor.execute(text)
-rows = cursor.fetchall()
-print(rows)
